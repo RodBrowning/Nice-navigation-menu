a611b73ea6e76ce209eaff1325aac0d98eb369c7
Rodrigo da Silva Moura <rodrigo.browning@gmail.com>
2023-05-24 00:13:23 -0300

    Nice navigation menu

---
+++ README.md
@@ -0,0 +1,24 @@
+---
+
+### Ativação do ESLint (Opcional)
+
+Se você deseja habilitar o ESLint, execute o seguinte comando para instalar suas dependências
+
+No entanto, este passo não é necessário para executar a aplicação.
+
+### Executando a Aplicação
+
+Para executar a aplicação, você tem duas opções:
+
+1. Abra o arquivo `index.html` diretamente em um navegador da web.
+2. Use uma extensão de servidor ao vivo (live server) em seu editor de código preferido.
+
+É recomendado usar uma extensão de servidor ao vivo, pois ela oferece recarregamento automático e outros recursos úteis. Aqui estão algumas extensões populares de servidor ao vivo:
+
+- [Live Server para Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+- [Live Reload para Sublime Text](https://packagecontrol.io/packages/LiveReload)
+- [Atom Live Server para Atom](https://atom.io/packages/atom-live-server)
+
+Escolha a opção que se adequa ao seu fluxo de trabalho e aproveite a aplicação!
+
+---
---
+++ index.js
@@ -0,0 +1,119 @@
+const navContainer = document.querySelector(".nav-container")
+navContainer.childNodes.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const itemIndex = Array.from(navContainer.children).indexOf(
+      event.currentTarget
+    )
+    translateSpaceship(itemIndex)
+    activeItemStyle(navContainer, event.currentTarget)
+  })
+})
+
+function activeItemStyle(navContainer, target) {
+  const activeItem = navContainer.querySelector(".item-active")
+  if (!!activeItem) activeItem.classList.remove("item-active")
+  target.classList.add("item-active")
+}
+
+// Todo -> Atribui os valores que controlam a animação
+setCSSPropertiesDefaltValues([
+  ["--default-translate-duration", "400ms"],
+  ["--animation-duration", "250ms"],
+])
+
+//! Desabilita a funcção enquanto o timeout estiver ativo para não interromper a animação
+let clickDisabled = false
+// Todo -> Usamos para saber quantos itens de distancia deveremos transitar
+let lastIndexClicked = 0
+
+function translateSpaceship(itemIndex) {
+  if (clickDisabled) return
+  clickDisabled = true
+
+  const root = document.querySelector(":root")
+  const rootVariables = getComputedStyle(root)
+  const menuWrapper = document.querySelector(".menu-wrapper")
+  const navContainer = menuWrapper.querySelector(".nav-container")
+  const spaceshipBox = menuWrapper.querySelector(".spaceship-box")
+  const numberOfItems = navContainer.childElementCount
+  const clickedElementIndex = itemIndex
+
+  menuWrapper.style.cursor = "not-allowed"
+
+  //* Get CSS variable values
+  const defaultLeftTranslateDuration = getCSSPropertyStringValue(
+    rootVariables,
+    "--default-translate-duration",
+    "ms"
+  )
+  const animationDuration = getCSSPropertyStringValue(
+    rootVariables,
+    "--animation-duration",
+    "ms"
+  )
+  //* Fim get CSS variable values
+
+  //* Define CSS variable values
+  // Todo -> Para manter uma velocidade constante pegamos o tempo que leva para transitar de
+  // Todo -- um item para outro e multiplicamos pelo numero de itens teremos de transitar
+  const leftTranslateTotalDuration = Math.abs(
+    defaultLeftTranslateDuration *
+      Math.abs(clickedElementIndex - lastIndexClicked)
+  )
+
+  // Todo -> Tempo que leva para a animação que acende a luz da nave se inicia
+  const animationDelay = `${Math.abs(
+    animationDuration - leftTranslateTotalDuration
+  ).toString()}ms`
+
+  // Todo -> Descobrimos a porcentagem que cada item ocupa
+  // Todo -- e muntiplicamos por quandos itens devemos transitar para ter a posição final do item
+  const translatePercentage = (100 / numberOfItems) * clickedElementIndex
+  root.style.setProperty("--left-translate", `${translatePercentage}%`)
+  //* Fim define CSS variable values
+
+  //* Set CSS variable values
+  root.style.setProperty(
+    "--translate-duration",
+    `${leftTranslateTotalDuration}ms`
+  )
+  // root.style.setProperty("--animation-duration", `${animationDuration}ms`)
+  root.style.setProperty("--animation-delay", animationDelay)
+  //* Fim set CSS variable values
+
+  //* Handle light animation class
+  if (spaceshipBox.classList.contains("animate"))
+    spaceshipBox.classList.remove("animate")
+  spaceshipBox.classList.add("animate")
+
+  //Todo -> Tempo total da transição
+  const time =
+    Math.abs(clickedElementIndex - lastIndexClicked) *
+    defaultLeftTranslateDuration
+
+  //Todo -> Remove classe após o fim da transição e reactiva a interação
+  setTimeout(() => {
+    spaceshipBox.classList.remove("animate")
+    clickDisabled = false
+    menuWrapper.style.cursor = "pointer"
+  }, time)
+  //* Fim handle toggle light animation class
+
+  lastIndexClicked = clickedElementIndex
+}
+
+function getCSSPropertyStringValue(rootVariables, variable, unit) {
+  return parseInt(rootVariables.getPropertyValue(variable).replace(unit, ""))
+}
+function getCSSPropertyDeciamalNumberValue(rootVariables, variable) {
+  return parseFloat(rootVariables.getPropertyValue(variable))
+}
+
+function setCSSPropertiesDefaltValues(varArray) {
+  const root = document.querySelector(":root")
+  varArray.forEach((variables) => {
+    const variableName = variables[0]
+    const variableValue = variables[1]
+    root.style.setProperty(variableName, variableValue)
+  })
+}
